d31fb717429dbc6b35069002e55b3891c19f34ba
Jason Lotito <jasonlotito@gmail.com>
2012-12-16 11:00:39 -0500

    Install and cleanup scripts work now

--- cleanup.sh
+++ cleanup.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
 
 # Cleans up the install
+echo "Cleaning up previous *.old files"
+echo $HOME
 
-files = ( '~/.vimrc.old' '~/.bashrc.old' '~/.bash_profile.old' '~/.gitconfig.old' )
-directories = ( '~/.git.old' )
+# Files and Directories that need to be cleaned up
+files=( '/.vimrc.old' '/.bashrc.old' '/.bash_profile.old' '/.gitconfig.old' )
+directories=( '/.git.old' )
 
+# Clean up all the files
 for file in "${files[@]}"
 do
-  if [ -f $file ]; then
+  file="$HOME$file"
+  if [ -f "$file" ]; then
+    echo "Removing $file"
     rm $file
   fi
 done
 
+# Clean up all the directories
 for directory in "${directories[@]}"
 do
-  if [ -d $directory ]; then
-    rm -rf $directory
+  directory="$HOME$directory"
+  if [ -d "$directory" ]; then
+    echo "Removing $directory"
+    if [ -L "$directory" ]; then
+      rm "$directory"
+    else
+      rm -rf "$directory"
+    fi
   fi
 done
+
+# Anything special can be done here
--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# First we want to cleanup previous installs
+# They will get overwritten regardless
+./cleanup.sh
+
 # VIM
+echo "Installing ~/.vimrc"
 if [ -f ~/.vimrc ]; then
   mv ~/.vimrc ~/.vimrc.old
 fi
@@ -8,6 +13,8 @@ fi
 ln -s ~/bin/dotfiles/vimrc ~/.vimrc
 
 # BASH
+echo "Installing ~/.bashrc"
+echo "Installing ~/.bash_profile"
 if [ -f ~/.bashrc ]; then
   mv ~/.bashrc ~/.bashrc.old
 fi
@@ -20,18 +27,25 @@ ln -s ~/bin/dotfiles/bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/bash_profile ~/.bash_profile
 
 # GIT
+echo "Installing ~/.gitconfig"
+echo "Installing ~/.git"
 if [ -f ~/.gitconfig ]; then
   mv ~/.gitconfig ~/.gitconfig.old
 fi
 
-if [ -d ~/.git ]; then
-  mv ~/.git ~/.git.old
+if [[ -d ~/.git || -L ~/.git ]]; then
+  if [ -L ~/.git ]; then
+    mv ~/.git ~/.git.old
+  else
+    mv ~/.git ~/.git.old
+  fi
 fi
 
 ln -s ~/bin/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/bin/dotfiles/git ~/.git
 
 # TMUX
+echo "Installing tmux.conf"
 if [ -f ~/.tmux.conf ]; then
   mv ~/.tmux.conf ~/.tmux.conf.old
 fi
@@ -39,5 +53,6 @@ fi
 ln -s ~/bin/dotfiles/tmux.conf ~/.tmux.conf
 
 # vcprompt
+echo "Installing vcprompot"
 curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ~/bin/vcprompt
 chmod 755 ~/bin/vcprompt
