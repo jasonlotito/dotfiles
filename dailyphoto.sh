a95d2815e665cf01fa4f72efa56306045edd9cc2
Jason Lotito <jasonlotito@gmail.com>
2013-01-07 21:13:33 -0500

    Adding dailyphoto.sh

---
+++ dailyphoto.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+IDLE=$((`/usr/sbin/ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/!{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000 ))
+export SUDO_ASKPASS=/Users/jasonlotito/bin/get_pass.sh
+if [ $IDLE -lt 61 ]; then
+    PID=`ps auxww  |grep loginwindow | grep -v grep | awk '{print $2}'`
+    D=`date +%Y%m%d_%H%M%S`
+    sudo -A /bin/launchctl bsexec $PID /Users/jasonlotito/bin/isightcapture -t png /Users/jasonlotito/daily_photo/$D.png
+fi
