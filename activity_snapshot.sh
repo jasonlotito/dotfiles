a4f3ebceb4363ff138ab4fd0120c6095742483d8
Jason Lotito <jasonlotito@gmail.com>
2013-01-08 07:03:00 -0500

    Only run scripts if screensaver is not active

--- activity_snapshot.sh
+++ activity_snapshot.sh
@@ -6,11 +6,14 @@ FILE="/Users/jasonlotito/bin/data/activity_log/`date +%Y%m%d`.log"
  
 function log_activity()
 {
-    local UTC=`date "+%s,%Z"`
-    local IDLE=$((`/usr/sbin/ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/!{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000 ))
-    local ACTIVE_PROGRAM=$(/usr/bin/osascript -e 'tell application "System Events"' -e 'set frontApp to name of first application process whose frontmost is true' -e 'end tell')
- 
-    echo "$UTC,$IDLE,$ACTIVE_PROGRAM" >> $FILE
+    ps ax | grep [S]creenSaverEngine > /dev/null
+    if [ "$?" != "0" ]; then
+        local UTC=`date "+%s,%Z"`
+        local IDLE=$((`/usr/sbin/ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/!{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000 ))
+        local ACTIVE_PROGRAM=$(/usr/bin/osascript -e 'tell application "System Events"' -e 'set frontApp to name of first application process whose frontmost is true' -e 'end tell')
+     
+        echo "$UTC,$IDLE,$ACTIVE_PROGRAM" >> $FILE
+    fi
 }
  
 log_activity
--- dailyphoto.sh
+++ dailyphoto.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+
 IDLE=$((`/usr/sbin/ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/!{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000 ))
+
 export SUDO_ASKPASS=/Users/jasonlotito/bin/get_pass.sh
+
 if [ $IDLE -lt 61 ]; then
-    PID=`ps auxww  |grep loginwindow | grep -v grep | awk '{print $2}'`
-    D=`date +%Y%m%d_%H%M%S`
-    sudo -A /bin/launchctl bsexec $PID /Users/jasonlotito/bin/isightcapture -t png /Users/jasonlotito/bin/data/daily_photo/$D.png
+    # Check to see if the Screen Saver is running
+    ps ax | grep [S]creenSaverEngine > /dev/null
+
+    if [ "$?" != "0" ]; then
+        PID=`ps auxww  |grep loginwindow | grep -v grep | awk '{print $2}'`
+        D=`date +%Y%m%d_%H%M%S`
+        sudo -A /bin/launchctl bsexec $PID /Users/jasonlotito/bin/isightcapture -t png /Users/jasonlotito/bin/data/daily_photo/$D.png
+    fi
 fi
