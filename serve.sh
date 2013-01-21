e8bbe1f249cf25baa9da838c78b2928243a0c29c
Jason Lotito <jasonlotito@gmail.com>
2013-01-21 09:08:09 -0500

    Improve serve so we can start and stop the server easily

--- serve.sh
+++ serve.sh
@@ -1,3 +1,68 @@
 #!/bin/sh
-echo "Setting up server at http://$HOSTNAME:8000/ for current directory"
-open http://$HOSTNAME:8000/ &&  python -m SimpleHTTPServer
+
+verbose=0
+start=0
+stop=0
+app="start"
+pid=0
+port=8000
+
+function startServer {
+	if [ $verbose = 1 ]
+		then
+		echo "Setting up server at http://$HOSTNAME:$port/ for current directory"
+		python -m SimpleHTTPServer &
+	else
+		python -m SimpleHTTPServer &>/dev/null &	
+	fi
+	echo $! > "/tmp/simple.server.$USER.$port"
+	
+	open http://$HOSTNAME:$port/ 	
+}
+
+function stopServer {
+	kill -HUP $(cat "/tmp/simple.server.$USER.$port")
+}
+
+function showHelp {
+	echo "
+Server
+version 0.1
+Author: Jason Lotito <jasonlotito@gmail.com>
+
+Commands
+	start 		Start server in current directory
+	stop 		Stop server
+
+Options
+	-v	 	Verbose output
+"
+}
+
+for arg in "$@"
+do
+	case "$arg" in
+		start)
+			app="start"
+		;;
+		stop) 
+			app="stop"
+		;;
+		help)
+			app="help"
+		;;
+		-v)
+			verbose=1
+	esac
+done
+
+case "$app" in
+	start)
+		startServer
+		;;
+	stop)
+		stopServer
+		;;
+	help)
+		showHelp
+esac
